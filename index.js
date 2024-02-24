fe28aac89435c39a41dcbb5b24b7cc8ff6703cf8
youssefhanin777 <122698298+youssefhanin777@users.noreply.github.com>
2024-02-24 19:42:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
-
-//-------------------------------------------------
-
         client.user.setActivity(statuses[i], {
             type: 'STREAMING',
             url: 'https://www.twitch.tv/youzarx'
         });
-//-------------------------------------------------
-
-
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
