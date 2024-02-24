776e2d6d6a7deac279f471973f41a9af87729d0f
youssefhanin777 <122698298+youssefhanin777@users.noreply.github.com>
2024-02-24 19:40:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 //-------------------------------------------------
-    const statuses = [
-        '🇵🇸 | Palestine.js'
-    ];
-    let i = 0;
-    setInterval(() => {
+
         client.user.setActivity(statuses[i], {
             type: 'STREAMING',
             url: 'https://www.twitch.tv/youzarx'
         });
-        i = ++i % statuses.length;
-    }, 1e4);
 //-------------------------------------------------
 
 
